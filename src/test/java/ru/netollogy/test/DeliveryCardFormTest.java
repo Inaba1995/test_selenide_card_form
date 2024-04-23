e1c1e14086ef93a5910b716b6d3eac855ef54d18
Inaba1995 <149956151+Inaba1995@users.noreply.github.com>
2024-04-23 21:32:38 +0300

    edit date field

--- src/test/java/ru/netollogy/test/DeliveryCardFormTest.java
+++ src/test/java/ru/netollogy/test/DeliveryCardFormTest.java
@@ -1,13 +1,13 @@
 package ru.netollogy.test;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -20,8 +20,9 @@ public class DeliveryCardFormTest {
         LocalDate date = LocalDate.now().plusDays(3);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         String text = date.format(formatter);
-        LocalDate parsedDate = LocalDate.parse(text, formatter);
-        $("[data-test-id='date']").$("[class='input__control']").setValue("parseDate");
+
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(text);
 
         $("[data-test-id='name']").$("[class='input__control']").setValue("Авдеева Елена Алексеевна");
         $("[data-test-id='phone']").$("[class='input__control']").setValue("+79991233443");
@@ -29,6 +30,8 @@ public class DeliveryCardFormTest {
 
         $(".button").click();
 
-        $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + text), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
 }
